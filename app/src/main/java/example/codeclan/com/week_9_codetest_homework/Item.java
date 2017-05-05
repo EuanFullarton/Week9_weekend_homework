8e9db48f777b1d790b79cf3de8cfc3e2b18108a9
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-05 11:43:09 +0100

    First test in ItemTest passing

--- app/src/main/java/example/codeclan/com/week_9_codetest_homework/Item.java
+++ app/src/main/java/example/codeclan/com/week_9_codetest_homework/Item.java
@@ -5,4 +5,18 @@ package example.codeclan.com.week_9_codetest_homework;
  */
 
 public class Item {
+
+    private String name;
+    private int value;
+
+        public Item(String name, int value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+
+
 }
--- app/src/test/java/example/codeclan/com/week_9_codetest_homework/ItemTest.java
+++ app/src/test/java/example/codeclan/com/week_9_codetest_homework/ItemTest.java
@@ -1,8 +1,25 @@
 package example.codeclan.com.week_9_codetest_homework;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 05/05/2017.
  */
 
 public class ItemTest {
+
+    Item item;
+
+    @Before
+    public void before() {
+        item = new Item("Chocolate", 1);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Chocolate", item.getName());
+    }
 }
