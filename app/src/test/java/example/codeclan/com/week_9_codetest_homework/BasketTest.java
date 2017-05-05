dbe29e09a78903aff8260a4d53726563fe99b8bc
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-05 12:50:04 +0100

    Was calling wrong method in test, tweaked slightly. Everything still working

--- app/src/test/java/example/codeclan/com/week_9_codetest_homework/BasketTest.java
+++ app/src/test/java/example/codeclan/com/week_9_codetest_homework/BasketTest.java
@@ -23,12 +23,12 @@ public class BasketTest {
         item1 = new Item("Chocolate", 1);
         item2 = new Item("Coffee", 2);
         items = new ArrayList<Item>(0);
-        basket = new Basket(items, 3);
+        basket = new Basket(items, 0);
     }
 
     @Test
     public void hasTotalValue(){
-        assertEquals(3, basket.getTotalValue());
+        assertEquals(0, basket.getTotalValue());
     }
 
     @Test
@@ -61,7 +61,7 @@ public class BasketTest {
     public void canGetTotalValue(){
         basket.addItem(item1);
         basket.addItem(item2);
-        assertEquals(3, basket.getTotalValue());
+        assertEquals(3, basket.calculateTotalValue());
     }
 
 }
