4b5e65206bb1e14dc163a62160d7301fd39103de
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-05 12:20:43 +0100

    basketStartsEmpty() and canAddItem() passing in BasketTest

--- app/src/main/java/example/codeclan/com/week_9_codetest_homework/Basket.java
+++ app/src/main/java/example/codeclan/com/week_9_codetest_homework/Basket.java
@@ -1,8 +1,43 @@
 package example.codeclan.com.week_9_codetest_homework;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 05/05/2017.
  */
 
 public class Basket {
+
+    private ArrayList<Item> items;
+    private int total_value;
+
+    public Basket(ArrayList<Item> items, int total_value){
+        this.items = items;
+        this.total_value = total_value;
+    }
+
+    public ArrayList<Item> getItems(){
+        return this.items;
+    }
+
+    public int getItemCount(){
+        return this.items.size();
+    }
+
+//    public int calculateTotalValue(){
+////        return the value of each item in arrayList
+//    }
+
+    public int getTotalValue(){
+        return this.total_value;
+    }
+
+    public void addItem(Item item){
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item){
+        this.items.remove(item);
+    }
+
 }
--- app/src/test/java/example/codeclan/com/week_9_codetest_homework/BasketTest.java
+++ app/src/test/java/example/codeclan/com/week_9_codetest_homework/BasketTest.java
@@ -1,8 +1,45 @@
 package example.codeclan.com.week_9_codetest_homework;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 05/05/2017.
  */
 
 public class BasketTest {
+
+    Item item1;
+    Item item2;
+    ArrayList<Item> items;
+    Basket basket;
+
+    @Before
+    public void before() {
+        item1 = new Item("Chocolate", 1);
+        item1 = new Item("Coffee", 2);
+        items = new ArrayList<Item>(0);
+        basket = new Basket(items, 3);
+    }
+
+    @Test
+    public void hasTotalValue(){
+        assertEquals(3, basket.getTotalValue());
+    }
+
+    @Test
+    public void basketStartsEmpty(){
+        assertEquals(0, basket.getItemCount());
+    }
+
+    @Test
+    public void canAddItem(){
+        basket.addItem(item1);
+        assertEquals(1, basket.getItemCount());
+    }
+  
 }
